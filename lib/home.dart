406d17797c26f2d5983bffed3e32c4e40e0391d2
Mehdi Raza <razamehdi2019@gmail.com>
2022-09-05 02:29:25 -0700

    done

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sharedprefrence/splash.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,9 +11,58 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+final emailcontroller = TextEditingController();
+final passwordcontroller = TextEditingController();
+final agecontroller = TextEditingController();
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Email'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Password'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Age'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: InkWell(
+                  onTap: () async {
+                    SharedPreferences sp =
+                        await SharedPreferences.getInstance();
+                    sp.setString('email', emailcontroller.text.toString());
+                    sp.setString(
+                        'Password', passwordcontroller.text.toString());
+                    sp.setString('age', agecontroller.text.toString());
+                    sp.setBool('login', true);
+                  },
+                  child: Container(
+                    height: 30,
+                    width: 600,
+                    decoration: BoxDecoration(
+                        color: Colors.teal,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(child: Text('perform')),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sharedprefrence/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -11,11 +15,42 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  void login() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    bool login = sp.getBool('login') ?? false;
+    if (login) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    } else {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => SplashScreen()));
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Image(
+          height: double.infinity,
+          fit: BoxFit.fitHeight,
           image: NetworkImage(
               'https://images.pexels.com/photos/5613271/pexels-photo-5613271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')),
     );
   }
 }
+
+// Future<void> mehdiRaza() async {
+//   await Future.delayed(Duration(
+//     seconds: 5,
+//   ));
+//   print('Dlayed');
+// }
